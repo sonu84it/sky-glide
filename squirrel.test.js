ba656b547a347097b53561319d030b05447279d4
aifirst <143290650+sonu84it@users.noreply.github.com>
2025-06-19 21:06:51 +0530

    Flip squirrel image orientation

--- squirrel.test.js
+++ squirrel.test.js
@@ -3,5 +3,5 @@ const path = require('path');
 
 test('squirrel image path is local', () => {
   const css = fs.readFileSync(path.join(__dirname, 'style.css'), 'utf8');
-  expect(css).toMatch(/background:\s*url\('assets\/squirrel\.svg'\)/);
+  expect(css).toMatch(/background:\s*url\('assets\/squirrel_right\.svg'\)/);
 });
